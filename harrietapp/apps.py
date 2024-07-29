f24a88c64a85e04c50040838d19a9a666cdc5c71
harrietenam <sabblahharriet11@gmail.com>
2024-07-29 09:27:24 -0400

    Added harriet Application

--- harrietapp/apps.py
+++ harrietapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangowebsiteConfig(AppConfig):
+class HarrietappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'djangowebsite'
+    name = 'harrietapp'
